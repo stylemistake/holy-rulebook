2143c477d704791907552c249e9e8da214406870
Style Mistake <stylemistake@gmail.com>
2018-07-06 22:22:12 +0300

    Serve the converted rulebook

--- src/server/setupRoutes.mjs
+++ src/server/setupRoutes.mjs
@@ -1,6 +1,12 @@
+import convert from '../rulebook/convert.js';
+
 export default function setupRoutes(router) {
-  // Assign routes here
-  // router.get('/', (req, res) => {
-  //   // ...
-  // })
+
+  console.log('Retrieving rulebook');
+  const rulebook = convert.getRulebookJson();
+
+  router.get('/rulebook', (req, res) => {
+    return res.send(rulebook);
+  });
+
 }
